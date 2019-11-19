be70af90ead67c3eac7a4173b49259f78225d2a1
gcm <gcaptainmarvel@gmail.com>
2019-11-19 21:57:45 +0200

    kreditobzor 1.0

--- kreditobzor/settings.py
+++ kreditobzor/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'static_root'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
