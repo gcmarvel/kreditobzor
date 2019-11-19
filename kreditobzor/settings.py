46262ef9317dbc6710918c57925d01a54614b457
gcm <gcaptainmarvel@gmail.com>
2019-11-19 10:17:00 +0200

    kreditobzor 1.0

--- kreditobzor/settings.py
+++ kreditobzor/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ux(li)*tegqx7#w2mc2!5qrth*)7l435d1gtm)9!39dz-k9az6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'frodo666.pythonanywhere.com']
