c96d716df7b502b0a9b9c2c978a084c7a39dc996
gcm <gcaptainmarvel@gmail.com>
2019-12-03 16:21:59 +0200

    kreditobzor 1.0

--- static/js/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('push', function(event) {
-  var message = JSON.parse(event.data.text()); //
-  event.waitUntil(
-    self.registration.showNotification(message.title, {
-      body: message.body,
-    })
-  );
-});
