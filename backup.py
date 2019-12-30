9576ce362c5e1cb7566e3d305c889482dd958137
gcm <gcaptainmarvel@gmail.com>
2019-12-30 23:18:38 +0200

    kreditobzor 1.1

---
+++ backup.py
@@ -0,0 +1,46 @@
+import datetime
+import smtplib
+import ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+today = datetime.date.today()
+weekday = today.weekday()
+
+if weekday == 0:
+
+    password = "tggkuvdwokxrpvaq"
+    sender = "kreditobzor_internal@mail.ru"
+    receiver = "kreditobzor@mail.ru"
+    body = ""
+
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = receiver
+    message["Subject"] = "Свежие базы данных завезли"
+    message.attach(MIMEText(body, "plain"))
+
+
+    context = ssl.create_default_context()
+
+    with open('db.sqlite3', 'rb') as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    encoders.encode_base64(part)
+
+    part.add_header(
+        "Content-Disposition",
+        "attachment; filename=db.sqlite3",
+    )
+
+    message.attach(part)
+
+    text = message.as_string()
+
+    with smtplib.SMTP_SSL("smtp.mail.ru", 465, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(sender, receiver, text)
